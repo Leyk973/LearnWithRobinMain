018e7085670cedb2d1cac1d3e1badb667c8ac659
JLMacarit <jl.macarit@ymail.com>
2018-03-15 12:15:49 +0100

    fin de seance

--- modsimon.h
+++ modsimon.h
@@ -5,7 +5,21 @@
 class ModSimon
 {
 public:
+
+    /// Constructeur destructeur
     ModSimon();
+    ~ModCalcul(void);
+
+    /// difficulte (de 1 facile à 3 difficile)
+    //void setDifficulte(int);
+
+    /// definir la séquence des couleurs
+    /// le parametre est le nombre de clignotements
+    void createSequence(int);
+
+private:
+    int seqNumbers[];
+
 };
 
 #endif // MODSIMON_H
