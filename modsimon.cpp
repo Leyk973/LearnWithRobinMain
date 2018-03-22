1456d981b6a7752a0c171922b2284d567df98cae
JLMacarit <jl.macarit@ymail.com>
2018-03-22 11:57:09 +0100

    [update] simon

--- modsimon.cpp
+++ modsimon.cpp
@@ -2,5 +2,15 @@
 
 ModSimon::ModSimon()
 {
-
+    this->lives=3;
+    this->score=0;
+    this->createSequence(1);
+    this->seqNum=1;
+    this->iniScoreToReach();
 }
+
+ModSimon::~ModSimon(void)
+{
+    // nada
+}
+
--- modsimon.h
+++ modsimon.h
@@ -1,6 +1,6 @@
 #ifndef MODSIMON_H
 #define MODSIMON_H
-
+#include "commonlibs.h"
 
 class ModSimon
 {
@@ -13,12 +13,53 @@ public:
     /// difficulte (de 1 facile à 3 difficile)
     //void setDifficulte(int);
 
+    /// SCORE
+    void setScore(int);
+    int getScore(void);
+
+    ///LIVES
+    /// Vérifier quand on la modifie qu'elle ne passe pas en dessous de 0
+    void setLives(int);
+    int getLives(void);
+    void addLife(void);
+    void remLife(void);
+
+
+    /// SEQUENCE
+    void setSequence(std::string);
+    std::string getSequence(void);
+
+    /// verifier séquence entrée
+    bool checkSequence (std::string);
+
     /// definir la séquence des couleurs
     /// le parametre est le nombre de clignotements
     void createSequence(int);
 
+    /// set le scoreToReach selon la difficulté
+    /// \todo implémenter difficulté
+    void iniScoreToReach(void);
+
 private:
-    int seqNumbers[];
+
+    /// difficulté
+    /// \todo implémenter
+    // std::string : difficulté
+
+    /// remaining lives
+    int lives;
+
+    /// current sequence
+    std::string sequence;
+
+    /// current sequence number
+    int seqNum;
+
+    /// score
+    int score;
+
+    /// score à atteindre
+    int scoreToReach;
 
 };
 
